d99f3d5a723c3b1ff790c94d4fc8d845119c4dcd
Gilnior <gilnior123@gmail.com>
2023-04-06 18:19:48 -0300

    feat(string_calculator): finish the first 5 steps of Kata 2 - String calculator

--- string_calculator/main.py
+++ string_calculator/main.py
@@ -1,2 +1,28 @@
+import re
+
 def add(input: str) -> int:
-    return 1
+    if not input:
+        return 0
+    separators = [',', '\n']
+
+    match_chosen_separator = re.match(r"//(\D+)\n", input)
+
+    if match_chosen_separator:
+        separators = [match_chosen_separator.group(1)]
+        prefix_to_remove = input[match_chosen_separator.start():match_chosen_separator.end()]
+        input = input.removeprefix(prefix_to_remove)
+
+    numbers = input.split(separators.pop())
+    for sep in separators:
+        numbers = sum([n.split(sep) for n in numbers], start=[])
+    
+    if '' in numbers:
+        raise ExceptionSeparatorAtTheEnd()
+
+    numbers_sum = 0
+    for n in numbers:
+        numbers_sum += int(n)
+    return numbers_sum
+
+class ExceptionSeparatorAtTheEnd(Exception):
+    ...
--- string_calculator/tests.py
+++ string_calculator/tests.py
@@ -1,4 +1,31 @@
-from .main import add
+from .main import add, ExceptionSeparatorAtTheEnd
+import pytest
 
 def test_takes_string_return_number():
     assert isinstance(add("0"), int)
+
+def test_return_zero_if_empty_string():
+    assert add("") == 0
+
+def test_handling_one_number():
+    assert add("99") == 99
+
+def test_handling_two_numbers():
+    assert add("99,1") == 100
+
+def test_handling_three_numbers():
+    assert add("1,2,3") == 6
+
+def test_handling_new_line_as_separator():
+    assert add("1\n2") == 3
+
+def test_handling_new_line_and_comma_as_separators():
+    assert add("3,3\n10") == 16
+
+def test_error_if_separator_at_the_end():
+    with pytest.raises(ExceptionSeparatorAtTheEnd):
+        add("2,4,5\n")
+
+def test_define_separator_at_the_start():
+    assert add("//;\n1;2") == 3
+    assert add("//sep\n2sep5") == 7
